5775cca736484770fe34733eb1c0e0582c798cd0
Kevan Lucchini <kevan.lucchini@gmail.com>
2019-09-02 18:13:31 -0400

    added comments

--- Longest_Substring_Without_Repeating_Characters.js
+++ Longest_Substring_Without_Repeating_Characters.js
@@ -1,3 +1,6 @@
+// Time Complexity: O(n) Each character of the string needs to be visited only once
+// Space Complexity: O(min(m.n)) Number of keys in dictionary is bounded by the alphabet m and size of string n
+
 function lengthOfLongestSubstring(s) {
     let windowCharsMap = {};
     let windowStart = 0;
@@ -5,6 +8,7 @@ function lengthOfLongestSubstring(s) {
 
     for (let i = 0; i < s.length; i ++) {
         const endChar = s[i];
+        
         if (windowCharsMap[endChar] >= windowStart) {
             windowStart = windowCharsMap[endChar] + 1;
         }
