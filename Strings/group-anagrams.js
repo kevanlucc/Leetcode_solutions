64d45d08d4de6e1421c15dbfa0cd6cd705d0d13a
Kevan Lucchini <kevan.lucchini@gmail.com>
2019-09-06 18:03:22 -0400

    Create group-anagrams.js

---
+++ Strings/group-anagrams.js
@@ -0,0 +1,21 @@
+/* 
+ Time Complexity: O(NMlogM) where N is number of strings and M is the length of strings 
+ Space Complexity: O(NK) size of the grouped hash map
+*/
+
+function groupAnagrams(strs) {
+    let grouped = [];
+
+    for (let i = 0; i < strs.length; i++) {
+        const word= strs[i];
+        const key = word.split('').sort().join();
+
+        if (!grouped[key]) { // if key not in group make an empty array and fill it
+            grouped[key] = []
+        };
+
+        grouped[key].push(word);
+    }
+
+    return Object.values(grouped);
+}
